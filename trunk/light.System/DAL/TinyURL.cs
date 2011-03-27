c558057428d5baef0bb72637b0d9b5cb87f37473
m@wanlei.net <m@wanlei.net@3946e055-d3ff-2030-1ece-352ffb7fd88b>
2011-03-27 15:48:01 +0000

    加入TinyURL模块

---
+++ trunk/light.System/DAL/TinyURL.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace light.System.DAL
+{
+    public sealed class TinyURL
+    {
+        public string GetCode(uint num)
+        { 
+        
+        }
+    }
+}
